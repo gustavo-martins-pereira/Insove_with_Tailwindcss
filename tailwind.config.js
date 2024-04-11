b23e9999f73f1d8b0e8103859c3d89acd1e6fd3a
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-04-11 15:48:40 -0300

    A&D - Hero & Unused Images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     content: ["./build/**/*.{html,js}"],
     theme: {
         extend: {
+            backgroundImage: {
+                "hero": "url('/build/assets/images/hero/hero.png')",
+            },
             fontFamily: {
                 "sans": ['"Poppins"', ...defaultTheme.fontFamily.sans],
             },
@@ -14,8 +17,11 @@ module.exports = {
             "primary": "#1CBCCF",
             "mine-shaft": "#333333",
             "abbey": "#4c5354",
+            "rolling-stone": "#777F81",
             "gray": "#808080",
-            "alto": "#DCDCDC"
+            "gull-gray": "#9AB4B7",
+            "alto": "#DCDCDC",
+            "mystic": "#E8F0F1",
         },
         boxShadow: {
             "3xl": "0 0 40px rgba(0, 0, 0, 0.1)"
