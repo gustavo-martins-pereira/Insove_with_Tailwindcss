da0fdc6574b03125f4caeca4be0c9c09080086af
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-04-11 14:16:33 -0300

    ADD - Header - 1/2

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
                 "sans": ['"Poppins"', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            "white": "#ffffff",
+            "primary": "#1CBCCF",
+            "mine-shaft": "#333333",
+            "abbey": "#4c5354",
+            "gray": "#808080",
+            "alto": "#DCDCDC"
+        },
+        boxShadow: {
+            "3xl": "0 0 40px rgba(0, 0, 0, 0.1)"
+        }
     },
     plugins: [],
 }
