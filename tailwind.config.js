003db95c9ac1511535e611cce59ed682e00430c8
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-04-13 14:31:00 -0300

    ADD - Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,25 +28,24 @@ module.exports = {
             screens: {
                 "msm": {"max": "640px"},
             },
-        },
-        // PROPERTIES
-        colors: {
-            "white": "#ffffff",
-            "primary": "#1CBCCF",
-            "secondary": "#90C6CD",
-            "mine-shaft": "#333333",
-            "abbey": "#4c5354",
-            "rolling-stone": "#777F81",
-            "gray": "#808080",
-            "gull-gray": "#9AB4B7",
-            "alto": "#DCDCDC",
-            "mystic": "#E8F0F1",
-        },
-        boxShadow: {
-            "3xl": "0 0 40px rgba(0, 0, 0, 0.1)",
-        },
-        backgroundPosition: {
-            "center-right": "center right",
+            colors: {
+                "white": "#ffffff",
+                "primary": "#1CBCCF",
+                "secondary": "#90C6CD",
+                "mine-shaft": "#333333",
+                "abbey": "#4c5354",
+                "rolling-stone": "#777F81",
+                "gray": "#808080",
+                "gull-gray": "#9AB4B7",
+                "alto": "#DCDCDC",
+                "mystic": "#E8F0F1",
+            },
+            boxShadow: {
+                "3xl": "0 0 40px rgba(0, 0, 0, 0.1)",
+            },
+            backgroundPosition: {
+                "center-right": "center right",
+            },
         },
     },
     plugins: [],
